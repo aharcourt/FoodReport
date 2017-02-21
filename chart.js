754996739042a2da48b7e3d4d00d64679f406f7b
Adeline Harcourt <aharcourt@users.noreply.github.com>
2017-02-20 21:41:13 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,35 @@
+var colors = ["#FFDAB9", "#E6E6FA", "#E0FFFF"];
+
+function drawSegment(canvas, context, i, data) {
+    context.save();
+    var centerX = Math.floor(canvas.width / 2);
+    var centerY = Math.floor(canvas.height / 2);
+    radius = Math.floor(canvas.width / 2);
+
+    var startingAngle = degreesToRadians(sumTo(data, i));
+    var arcSize = degreesToRadians(data[i]);
+    var endingAngle = startingAngle + arcSize;
+
+    context.beginPath();
+    context.moveTo(centerX, centerY);
+    context.arc(centerX, centerY, radius, 
+                startingAngle, endingAngle, false);
+    context.closePath();
+
+    context.fillStyle = colors[i];
+    context.fill();
+
+    context.restore();
+}
+
+function degreesToRadians(degrees) {
+    return (degrees * Math.PI)/180;
+}
+
+function sumTo(a, i) {
+    var sum = 0;
+    for (var j = 0; j < i; j++) {
+      sum += a[j];
+    }
+    return sum;
+}
